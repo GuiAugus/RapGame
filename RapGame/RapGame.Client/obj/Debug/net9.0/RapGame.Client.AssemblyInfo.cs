60076684d6ee0c79a0198382a3768ca06ac2921d
Gui <augusto.g@outlook.com.br>
2025-04-02 16:28:51 -0300

    criacao models de albuns, artistas e suas relacoes

--- RapGame/RapGame.Client/obj/Debug/net9.0/RapGame.Client.AssemblyInfo.cs
+++ RapGame/RapGame.Client/obj/Debug/net9.0/RapGame.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RapGame.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2f63595e85a1747a0a7c7307d06941f5e4fe3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("RapGame.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RapGame.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RapGame/RapGame/Data/RapGameDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RapGame.Models;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace RapGame.Data
+{
+    public class RapGameDbContext : DbContext
+    {
+        public DbSet<Album> Albuns { get; set; }
+        public DbSet<Artista> Artistas { get; set; }
+        public DbSet<AlbumArtista> AlbumArtistas { get; set; } // Corrigido o nome da propriedade
+
+        public RapGameDbContext(DbContextOptions<RapGameDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Relacionamento principal (Artista <-> Album)
+            modelBuilder.Entity<AlbumArtista>()
+                .HasKey(aa => new { aa.AlbumId, aa.ArtistId });
+
+            modelBuilder.Entity<AlbumArtista>()
+                .HasOne(aa => aa.Album)
+                .WithMany(a => a.AlbumArtistas)
+                .HasForeignKey(aa => aa.AlbumId);
+
+            modelBuilder.Entity<AlbumArtista>()
+                .HasOne(aa => aa.Artista)
+                .WithMany(a => a.AlbumArtistas)
+                .HasForeignKey(aa => aa.ArtistId);
+
+            // Relacionamento de participações (Artista convidado <-> Album)
+            modelBuilder.Entity<AlbumParticipacao>()
+                .HasKey(ap => new { ap.AlbumId, ap.ArtistaId });
+
+            modelBuilder.Entity<AlbumParticipacao>()
+                .HasOne(ap => ap.Album)
+                .WithMany(a => a.AlbumParticipacoes)
+                .HasForeignKey(ap => ap.AlbumId);
+
+            modelBuilder.Entity<AlbumParticipacao>()
+                .HasOne(ap => ap.Artista)
+                .WithMany(a => a.AlbumParticipacoes)
+                .HasForeignKey(ap => ap.ArtistaId);
+        }
+    }
+}
